cee53bce599357caa124034aeccb5d40597f3855
blagovest.yuriev.petrov <blagovest.yuriev.petrov@gmail.com>
2021-04-07 21:15:34 +0300

    first

---
+++ React-For-Beginners-Starter-Files/catch-of-the-day/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {render} from 'react-dom';
+import {StorePicker} from '../src/components/storePicker.js';
+import App from './components/App.js';
+import './css/style.css';
+
+
+render(<App/>,document.querySelector('#main'));
