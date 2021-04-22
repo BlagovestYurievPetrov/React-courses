f609b7b832f7e33c7e4e125b2e9aef71368abd4c
blagovest.yuriev.petrov <blagovest.yuriev.petrov@gmail.com>
2021-04-22 21:08:43 +0300

    setup routing via react router

--- Learn-Redux-Starter-Files-master/learn-redux/client/reduxstagram.js
+++ Learn-Redux-Starter-Files-master/learn-redux/client/reduxstagram.js
@@ -2,5 +2,17 @@ import React from 'react';
 import {render} from 'react-dom';
 import css from './styles/style.styl';
 import Main from './components/Main.js';
+import Single from './components/Single.js';
+import PhotoGrid from './components/PhotoGrid.js';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 
-render(<Main/>,document.getElementById('root'));
+const router = (
+    <Router history={browserHistory}>
+        <Route path="/" component={Main}>
+            <IndexRoute component={PhotoGrid}></IndexRoute>
+            <Route path="/view/:postId" component={Single}></Route>
+        </Route>
+    </Router>
+)
+
+render(router,document.getElementById('root'));
