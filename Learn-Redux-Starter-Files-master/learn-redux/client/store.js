58f10c57cd439627e5be887ea573dea3176800d8
blagovest.yuriev.petrov <blagovest.yuriev.petrov@gmail.com>
2021-04-24 14:05:09 +0300

    creating redux store

---
+++ Learn-Redux-Starter-Files-master/learn-redux/client/store.js
@@ -0,0 +1,16 @@
+import { createStore, compose } from 'redux';
+import { syncHistoryWithStore} from 'react-router-redux';
+import { browserHistory } from 'react-router';
+
+import rootReducer from './reducers/index.js';
+
+import comments from './data/comments.js';
+import posts from './data/posts.js';
+
+const defaultState = {
+    posts,
+    comments
+}
+
+export const store = createStore(rootReducer, defaultState);
+export const history = syncHistoryWithStore(browserHistory, store);
