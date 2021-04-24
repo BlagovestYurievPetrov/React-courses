c059f7edd004c3c4794e5b2a2724f25f45ff33dc
blagovest.yuriev.petrov <blagovest.yuriev.petrov@gmail.com>
2021-04-24 13:39:20 +0300

    Removed credentials

--- React-For-Beginners-Starter-Files/catch-of-the-day/src/base.js
+++ React-For-Beginners-Starter-Files/catch-of-the-day/src/base.js
@@ -2,9 +2,9 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyDmbwFxxhzy3iN4uRsmg5l4Reji06Uch64",
-    authDomain: "cotd-b-petrov.firebaseapp.com",
-    databaseURL: "https://cotd-b-petrov-default-rtdb.firebaseio.com"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: ""
 })
 
 const base = Rebase.createClass(firebaseApp.database());
