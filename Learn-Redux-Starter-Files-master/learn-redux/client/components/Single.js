f9a0ba5ccf9a4dc60081a23a968f5a0cd31a30b4
blagovest.yuriev.petrov <blagovest.yuriev.petrov@gmail.com>
2021-04-25 22:00:45 +0300

    add and remove comments

--- Learn-Redux-Starter-Files-master/learn-redux/client/components/Single.js
+++ Learn-Redux-Starter-Files-master/learn-redux/client/components/Single.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Photo from './Photo.js';
+import Comments from './Comments.js';
 
 const Single = React.createClass({
     render() {
+        const {postId} = this.props.params;
+        const i = this.props.posts.findIndex((post) => post.code === postId);
+        const post = this.props.posts[i];
+        const postComments = this.props.comments[postId] || []; 
         return (
             <div className="single-photo">
-                Single
+                <Photo i={i} post={post} {...this.props}></Photo>
+                <Comments postComments={postComments} {...this.props}/>
             </div>
         )
     }
