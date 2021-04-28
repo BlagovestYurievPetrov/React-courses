21d018f2abaa5cbb10c9b71d0a9a9343f73d6158
blagovest.yuriev.petrov <blagovest.yuriev.petrov@gmail.com>
2021-04-28 21:51:24 +0300

    pages

---
+++ Advanced-React-master/Advanced-React-master/sick-fits/frontend/pages/orders.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Orders extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Orders</p>
+      </div>
+    );
+  }
+}
+
+export default Orders;
---
+++ Advanced-React-master/Advanced-React-master/sick-fits/frontend/pages/products.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Products extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>Products</p>
+      </div>
+    );
+  }
+}
+
+export default Products;
