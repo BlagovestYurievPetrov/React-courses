df5a63b6041dd11ca28567611c95430eea7cfe95
blagovest.yuriev.petrov <blagovest.yuriev.petrov@gmail.com>
2021-05-20 22:31:00 +0300

    sign up

--- Advanced-React-master/Advanced-React-master/sick-fits/frontend/components/SignUp.js
+++ Advanced-React-master/Advanced-React-master/sick-fits/frontend/components/SignUp.js
@@ -5,51 +5,50 @@ import useForm from '../lib/useForm';
 import { CURRENT_USER_QUERY } from './User';
 import Error from './ErrorMessage';
 
-const SIGNIN_MUTATION = gql`
-  mutation SIGNIN_MUTATION($email: String!, $password: String!) {
-    authenticateUserWithPassword(email: $email, password: $password) {
-      ... on UserAuthenticationWithPasswordSuccess {
-        item {
-          id
-          email
-          name
-        }
-      }
-      ... on UserAuthenticationWithPasswordFailure {
-        code
-        message
-      }
+const SIGNUP_MUTATION = gql`
+  mutation SIGNUP_MUTATION(
+    $email: String!
+    $name: String!
+    $password: String!
+  ) {
+    createUser(data: { email: $email, name: $name, password: $password }) {
+      id
+      email
+      name
     }
   }
 `;
 
-export default function SignIn() {
+export default function SignUp() {
   const { inputs, handleChange, resetForm } = useForm({
     email: '',
+    name: '',
     password: '',
   });
-  const [signin, { data, loading }] = useMutation(SIGNIN_MUTATION, {
+  const [signup, { data, loading, error }] = useMutation(SIGNUP_MUTATION, {
     variables: inputs,
-    refetchQueries: [{ query: CURRENT_USER_QUERY }],
+    // refetchQueries: [{ query: CURRENT_USER_QUERY }],
   });
 
   async function handleSubmit(e) {
     e.preventDefault();
     // send email and pass to grahql api
-    const res = await signin();
+    const res = await signup().catch(console.error);
     console.log(res);
+    console.log({ data, loading, error });
     resetForm();
   }
-  const error =
-    data?.authenticateUserWithPassword.__typename ===
-    'UserAuthenticationWithPasswordFailure'
-      ? data?.authenticateUserWithPassword
-      : undefined;
+
   return (
     <Form method="POST" onSubmit={handleSubmit}>
-      <h2>Sign into your account</h2>
+      <h2>Sign up for an account</h2>
       <Error error={error} />
       <fieldset>
+        {data?.createUser && (
+          <p>
+            Signed up with {data.createUser.email} - Please go ahead and sign in
+          </p>
+        )}
         <label htmlFor="email">
           Email
           <input
@@ -58,6 +57,17 @@ export default function SignIn() {
             placeholder="Your email adress"
             autoComplete="email"
             value={inputs.email}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="email">
+          Your name
+          <input
+            type="text"
+            name="name"
+            placeholder="Your name"
+            autoComplete="Name"
+            value={inputs.name}
             onChange={handleChange}
           />
         </label>
@@ -72,7 +82,7 @@ export default function SignIn() {
             onChange={handleChange}
           />
         </label>
-        <button type="submit">Sign in</button>
+        <button type="submit">Sign up</button>
       </fieldset>
     </Form>
   );
