5cb9b822c7514f1d8d550ddc7cccce871547e049
blagovest.yuriev.petrov <blagovest.yuriev.petrov@gmail.com>
2021-05-24 16:25:05 +0300

    cartState.js

---
+++ Advanced-React-master/Advanced-React-master/sick-fits/frontend/lib/cartState.js
@@ -0,0 +1,35 @@
+import { createContext } from 'react';
+import { useContext, useState } from 'react/cjs/react.development';
+
+const LocalStateContext = createContext();
+const LocalStateProvider = LocalStateContext.Provider;
+
+function CartStateProvider({ children }) {
+  const [cartOpen, setCartOpen] = useState(false);
+
+  function toggleCart() {
+    setCartOpen(!cartOpen);
+  }
+
+  function closeCart() {
+    setCartOpen(false);
+  }
+
+  function openCart() {
+    setCartOpen(true);
+  }
+
+  return (
+    <LocalStateProvider
+      value={{ cartOpen, setCartOpen, toggleCart, closeCart, openCart }}
+    >
+      {children}
+    </LocalStateProvider>
+  );
+}
+
+function useCart() {
+  const all = useContext(LocalStateContext);
+  return all;
+}
+export { CartStateProvider, useCart };
