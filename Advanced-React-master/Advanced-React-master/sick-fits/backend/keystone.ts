4613cce665e13a9a5cedacd64497b0a043a88bda
blagovest.yuriev.petrov <blagovest.yuriev.petrov@gmail.com>
2021-05-24 21:38:12 +0300

    adding to cart

--- Advanced-React-master/Advanced-React-master/sick-fits/backend/keystone.ts
+++ Advanced-React-master/Advanced-React-master/sick-fits/backend/keystone.ts
@@ -1,18 +1,25 @@
-/*eslint-disable */
-import 'dotenv/config';
-import { config, createSchema } from '@keystone-next/keystone/schema';
-import { User } from './schemas/User';
-import { Product } from './schemas/Product';
-import { CartItem } from './schemas/CartItem';
 import { createAuth } from '@keystone-next/auth';
-import { withItemData, statelessSessions } from '@keystone-next/keystone/session';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import {
+    withItemData,
+    statelessSessions,
+} from '@keystone-next/keystone/session';
+import { CartItem } from './schemas/CartItem';
 import { ProductImage } from './schemas/ProductImage';
+import { Product } from './schemas/Product';
+import { User } from './schemas/User';
+import 'dotenv/config';
 import { insertSeedData } from './seed-data';
 import { sendPasswordResetEmail } from './lib/mail';
+import { extendGraphqlSchema } from './mutations';
 
-const databaseURL = process.env.DATABASE_URL;
+function check(name: string) { }
+
+const databaseURL =
+    process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
 const sessionConfig = {
-    maxAge: 60 * 60 * 24 * 360,
+    maxAge: 60 * 60 * 24 * 360, // How long they stay signed in?
     secret: process.env.COOKIE_SECRET,
 };
 
@@ -21,46 +28,53 @@ const { withAuth } = createAuth({
     identityField: 'email',
     secretField: 'password',
     initFirstItem: {
-        fields: ['name', 'email', 'password']
-        //TODO: add in initial roles
+        fields: ['name', 'email', 'password'],
+        // TODO: Add in inital roles here
     },
     passwordResetLink: {
         async sendToken(args) {
+            // send the email
             await sendPasswordResetEmail(args.token, args.identity);
-        }
-    }
-})
-export default withAuth(config({
-    server: {
-        cors: {
-            origin: [process.env.FRONTEND_URL],
-            credentials: true,
         },
     },
-    db: {
-        adapter: 'mongoose',
-        url: databaseURL,
-        async onConnect(keystone) {
-            console.log('connected');
-            if (process.argv.includes('--seed-data')) {
-                await insertSeedData(keystone);
-            }
-        }
-    },
-    lists: createSchema({
-        // Schema items go in here
-        User,
-        Product,
-        ProductImage,
-        CartItem,
-    }),
-    ui: {
-        // TODO: Change this for roles
-        isAccessAllowed: ({ session }) => {
-            return !!session?.data
+});
+
+export default withAuth(
+    config({
+        // @ts-ignore
+        server: {
+            cors: {
+                origin: [process.env.FRONTEND_URL],
+                credentials: true,
+            },
         },
-    },
-    session: withItemData(statelessSessions(sessionConfig), {
-        User: `id`
+        db: {
+            adapter: 'mongoose',
+            url: databaseURL,
+            async onConnect(keystone) {
+                console.log('Connected to the database!');
+                if (process.argv.includes('--seed-data')) {
+                    await insertSeedData(keystone);
+                }
+            },
+        },
+        lists: createSchema({
+            // Schema items go in here
+            User,
+            Product,
+            ProductImage,
+            CartItem,
+        }),
+        extendGraphqlSchema,
+        ui: {
+            // Show the UI only for poeple who pass this test
+            isAccessAllowed: ({ session }) =>
+                // console.log(session);
+                !!session?.data,
+        },
+        session: withItemData(statelessSessions(sessionConfig), {
+            // GraphQL Query
+            User: 'id name email',
+        }),
     })
-}));
+);
