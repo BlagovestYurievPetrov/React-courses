c662e5e35d94d1bb3065401db6b97707eacc6b8a
blagovest.yuriev.petrov <blagovest.yuriev.petrov@gmail.com>
2021-05-28 08:20:28 +0300

    checkout ui

--- Advanced-React-master/Advanced-React-master/sick-fits/backend/mutations/checkout.ts
+++ Advanced-React-master/Advanced-React-master/sick-fits/backend/mutations/checkout.ts
@@ -87,7 +87,7 @@ id
 
     });
 
-    const cartItemIds = cartItems.map(cartItem => cartItem.id);
+    const cartItemIds = user.cart.map(cartItem => cartItem.id);
     await context.lists.CartItem.deleteMany({
         ids: cartItemIds
     });
