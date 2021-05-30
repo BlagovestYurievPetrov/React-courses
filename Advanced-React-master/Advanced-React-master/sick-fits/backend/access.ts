561a6fcf42802f47ada9af23d1a26f71d3c8a7f7
blagovest.yuriev.petrov <blagovest.yuriev.petrov@gmail.com>
2021-05-30 15:29:38 +0300

    rules

--- Advanced-React-master/Advanced-React-master/sick-fits/backend/access.ts
+++ Advanced-React-master/Advanced-React-master/sick-fits/backend/access.ts
@@ -1,5 +1,35 @@
+import { permissionsList } from './schemas/fields';
 import { ListAccessArgs } from './types';
 
 export function isSignedIn({ session }: ListAccessArgs) {
     return !!session;
 }
+
+const generatedPermissions = Object.fromEntries(
+    permissionsList.map((permission) => [
+        permission,
+        function ({ session }: ListAccessArgs) {
+            return !!session?.data.role?.[permission];
+        },
+    ])
+);
+
+export const permissions = {
+    ...generatedPermissions,
+};
+
+// rules
+export const rules = {
+    canManageProducts({ session }: ListAccessArgs) {
+        if (permissions.canManageProducts({ session })) {
+            return true;
+        }
+        return { user: { id: session.itemId } };
+    },
+    canReadProducts({ session }: ListAccessArgs) {
+        if (permissions.canManageProducts({ session })) {
+            return true;
+        }
+        return { status: 'AVAILABLE' };
+    },
+};
--- Advanced-React-master/Advanced-React-master/sick-fits/backend/schemas/Product.ts
+++ Advanced-React-master/Advanced-React-master/sick-fits/backend/schemas/Product.ts
@@ -1,14 +1,14 @@
 /* eslint-disable */
 import { integer, relationship, select, text } from '@keystone-next/fields';
 import { list } from '@keystone-next/keystone/schema';
-import { isSignedIn } from '../access';
+import { isSignedIn, rules } from '../access';
 
 export const Product = list({
     access: {
         create: isSignedIn,
-        read: isSignedIn,
-        update: isSignedIn,
-        delete: isSignedIn,
+        read: rules.canReadProducts,
+        update: rules.canManageProducts,
+        delete: rules.canManageProducts,
     },
     fields: {
         name: text({ isRequired: true }),
@@ -30,6 +30,10 @@ export const Product = list({
             }
         }),
         price: integer(),
+        user: relationship({
+            ref: 'User.products',
+            defaultValue: ({ context }) => ({ connect: { id: context.session.itemId } })
+        }),
         photo: relationship({
             ref: 'ProductImage.product',
             ui: {
